a9b21712b17a87da62249879695f4349444e35c2
melisa-gisel-gimenez <melisag@live.com.ar>
2021-03-02 23:02:56 +0000

    modifique views post_list_1.html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+def post_list_1(request):
+    return render(request, 'blog/post_list_1.html', {})
 
