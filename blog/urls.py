d4f8e5dc862637471e9a82e9308ec626441fe456
melisa-gisel-gimenez <melisag@live.com.ar>
2021-03-08 05:07:17 +0000

    se crean nuevas url

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list_1, name='post_list_1'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
